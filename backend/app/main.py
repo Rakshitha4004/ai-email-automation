aaba8f0f5ff222ffd615c821dff45d3830b01b0b
Rakshitha Pawar <pawarrakshitha34@gmail.com>
2026-05-19 20:23:22 +0530

    Allow all frontend origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,18 +6,12 @@ from app.auth.token_store import save_credentials, load_credentials
 from app.services.gmail_service import fetch_emails, create_gmail_draft
 from app.services.ai_service import classify_email, generate_reply
 
-from google_auth_oauthlib.flow import Flow
-from googleapiclient.discovery import build
-
 app = FastAPI()
 
 # CORS FIX
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://ai-email-automation-rust.vercel.app",
-        "http://localhost:3000",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -27,8 +21,9 @@ app.add_middleware(
 def home():
     return {"message": "AI Email Automation API Running"}
 
+
 @app.get("/auth/google/login")
-def login():
+def google_login():
     flow = create_flow()
 
     authorization_url, state = flow.authorization_url(
@@ -38,8 +33,9 @@ def login():
 
     return {"auth_url": authorization_url}
 
+
 @app.get("/auth/google/callback")
-def callback(code: str):
+def google_callback(code: str):
     flow = create_flow()
 
     flow.fetch_token(code=code)
@@ -48,7 +44,8 @@ def callback(code: str):
 
     save_credentials(credentials)
 
-    return {"message": "Login successful"}
+    return {"message": "Google login successful"}
+
 
 @app.get("/emails")
 def get_emails():
@@ -72,6 +69,7 @@ def get_emails():
 
     return classified_emails
 
+
 @app.get("/generate-reply")
 def generate_ai_reply(subject: str, sender: str):
     reply = generate_reply(subject, sender)
@@ -81,6 +79,7 @@ def generate_ai_reply(subject: str, sender: str):
         "from": sender,
         "reply": reply
     }
+
 
 @app.get("/create-draft")
 def create_draft(subject: str, sender: str, reply: str):
